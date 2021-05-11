07bea29c9ff9add6c2423fd4e66e263cafe15525
Bhupendrashahu <76242175+Bhupendrashahu@users.noreply.github.com>
2021-05-11 18:22:41 +0530

    Add files via upload

---
+++ sum of odd no.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i,a=0;
+	cout<<"enter the no\n  ";
+	cin>>n;
+	for(i=0;i<=n;i++)
+	{
+		if(i%2!=0){
+		
+		a=a+i;
+	
+	}
+	}
+		cout<<a<<"\n";
+}
